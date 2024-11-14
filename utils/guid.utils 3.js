2377768dc324908e208ec5d775fa13329ecde590
Haren Lin <allen880530@gmail.com>
2024-11-14 09:26:20 -0800

    feat-dev: 30 sec based inclusivity check

--- utils/guid.utils 3.js
+++
@@ -1,6 +0,0 @@
-// Function to generate a unique 4-character GUID
-function generateGUID() {
-    return Math.random().toString(36).substring(2, 6).toUpperCase();
-}
-
-module.exports = { generateGUID };
